83d31385e207f9e13f0ab27ada54dc2aa98878f1
dawit2123 <dawitzewdu2123@gmail.com>
2024-01-30 10:41:42 +0300

    reading and writing files in asychronous way added

--- Node Farm/index.js
+++ Node Farm/index.js
@@ -11,5 +11,20 @@ const http = require("http");
 //   path.join(__dirname, "./txt/writedFile.txt"),
 //   `The Written file document is ${readedFile}\n Document creaded time ${Date.now()}`
 // );
-
+// reading and writing the file in asynchronous way
+// fs.readFile(
+//   path.join(__dirname, "./txt/read-this.txt"),
+//   "utf-8",
+//   (err, data1) => {
+//     if (err) console.log(err);
+//     fs.writeFile(
+//       path.join(__dirname, "/txt/writtenFileAsync.txt"),
+//       `The file that is written asynchronously is: ${data1} \n Created at ${Date.now()}`,
+//       (err) => {
+//         if (err) console.log(err);
+//         console.log("File written successfully");
+//       }
+//     );
+//   }
+// );
 ////////////////////////////////////SERVER////////////////////////////////////
