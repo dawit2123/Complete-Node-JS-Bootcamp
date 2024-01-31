e5975f8ba356fd6760d9bb8f77bf8733ab9d97fe
dawit2123 <dawitzewdu2123@gmail.com>
2024-01-31 11:28:35 +0300

    thread pool size set to 8

--- How Node Works/event-loop.js
+++ How Node Works/event-loop.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 setTimeout(() => console.log("setTimeout beginner"), 0);
 setImmediate(() => console.log("setImmediate beginner"));
 //this is when the event loop events get into some competition based on rules
+//Setting a thread pool size to 8 (overriding the default 4 thread pool size)
+//process.env.UV_THREAD_POOL_SIZE=8;
 fs.readFile(`${__dirname}/test-file.txt`, (err, data) => {
   console.log("reading file started");
   setTimeout(() => console.log("setTimeout finished"), 0);
