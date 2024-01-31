dd864a15fccf1805ec68a4bd094f38af54b0056f
dawit2123 <dawitzewdu2123@gmail.com>
2024-01-31 11:19:49 +0300

    process.nextTick

--- How Node Works/event-loop.js
+++ How Node Works/event-loop.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 setTimeout(() => console.log("setTimeout beginner"), 0);
 setImmediate(() => console.log("setImmediate beginner"));
+//this is when the event loop events get into some competition based on rules
 fs.readFile(`${__dirname}/test-file.txt`, (err, data) => {
   console.log("reading file started");
-  setTimeout(() => console.log("setTimeout final"), 0);
-  setTimeout(() => console.log("setTimeout final"), 3000);
-  setImmediate(() => console.log("setImmediate final"));
+  setTimeout(() => console.log("setTimeout finished"), 0);
+  setTimeout(() => console.log("setTimeout finished"), 3000);
+  setImmediate(() => console.log("setImmediate finished"));
+  process.nextTick(() => console.log("process.nextTick finished"));
 });
