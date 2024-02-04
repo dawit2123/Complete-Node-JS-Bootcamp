9d30ab198e10b5f43a681c2edb47c54fc55a2742
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-05 02:14:05 +0300

    signup using jwt done

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -1,6 +1,6 @@
 const catchASync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
-
+const JWT = require('jsonwebtoken');
 module.exports.signup = catchASync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -8,8 +8,12 @@ module.exports.signup = catchASync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   });
+  const token = JWT.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
