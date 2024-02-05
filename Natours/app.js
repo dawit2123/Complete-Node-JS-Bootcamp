e001c864513361ac97ba1474ac0dc318c480b703
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-05 23:19:22 +0300

    prevented against the parameter pollution attacks

--- Natours/app.js
+++ Natours/app.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 //serving the static files
@@ -34,6 +35,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 //sanitizing against the xss attacks
 app.use(xss());
+//preventing the parameter pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 //A handler for the pages that are found in the routes
