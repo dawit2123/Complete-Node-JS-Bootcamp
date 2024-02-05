c21d3249de707ccb70a9e917646974a7b5be808d
dawit2123 <dawitzewdu2123@gmail.com>
2024-02-05 23:06:25 +0300

    rate limit done for the api routes and the login routes

--- Natours/app.js
+++ Natours/app.js
@@ -10,6 +10,19 @@ const app = express();
 //serving the static files
 app.use(express.static(path.join(__dirname, 'public')));
 //a middlware for setting a limit for the request from a specific ip
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this pc. Please try again in one hour'
+});
+app.use('/api', limiter);
+//a middleware for limiting attempts to the login page
+const limiterLogin = rateLimit({
+  max: 4,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this pc. Please try again in one hour'
+});
+app.use('/api/v1/users/login', limiterLogin);
 //Adding a middleware to the body object
 app.use(express.json({ limit: '10kb' }));
 app.use('/api/v1/tours', tourRouter);
